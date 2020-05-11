e745c58877f777e8418789d0888918721a3076f0
zhaidengke <zhaidengke-jk@360jinrong.net>
2020-05-11 19:45:53 +0800

    张艳

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>张艳艳</code>
+          <code>张艳</code>
         </p>
         <a
           className="App-link"
